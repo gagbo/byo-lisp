dfb1b5a6b713409c478478cef44f1b3c1cf473f5
Gerry Agbobada <gagbobada+git@gmail.com>
2018-05-13 17:03:48 +0200

    Evaluation : remove dead code

--- src/evaluation.c
+++ src/evaluation.c
@@ -9,54 +9,6 @@
 #include "mpc.h"
 
 struct lval*
-eval(mpc_ast_t* a) {
-    if (strstr(a->tag, "number")) {
-        errno = 0;
-        double x = strtod(a->contents, NULL);
-        return errno != ERANGE ? lval_num(x) : lval_err("invalid number");
-    }
-
-    /* In this case (!number), 0 is '(' */
-    char* op = a->children[1]->contents;
-
-    struct lval* x = eval(a->children[2]);
-
-    int i = 3;
-    while (strstr(a->children[i]->tag, "expr")) {
-        x = eval_op(x, op, eval(a->children[i]));
-        ++i;
-    }
-
-    return x;
-}
-
-struct lval*
-eval_op(struct lval* x, char* op, struct lval* y) {
-    if (x->type == LVAL_ERR) {
-        return x;
-    }
-    if (y->type == LVAL_ERR) {
-        return y;
-    }
-
-    if (strcmp(op, "+") == 0) {
-        return lval_num(x->num + y->num);
-    }
-    if (strcmp(op, "-") == 0) {
-        return lval_num(x->num - y->num);
-    }
-    if (strcmp(op, "*") == 0) {
-        return lval_num(x->num * y->num);
-    }
-    if (strcmp(op, "/") == 0) {
-        return y->num == 0 ? lval_err("division by zero")
-                           : lval_num(x->num / y->num);
-    }
-
-    return lval_err("bad operator detected");
-}
-
-struct lval*
 builtin_op(struct lval* a, char* op) {
     if (a->type == LVAL_ERR) {
         return a;
--- src/evaluation.h
+++ src/evaluation.h
@@ -4,9 +4,6 @@
 #include "lval.h"
 #include "mpc.h"
 
-struct lval* eval(mpc_ast_t* a);
-struct lval* eval_op(struct lval* x, char* op, struct lval* y);
-
 struct lval* builtin_op(struct lval* x, char* op);
 
 #endif  // EVALUATION_H_
