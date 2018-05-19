4144b4026d0dc9d80ec414f3713eb7838ed7593a
Gerry Agbobada <gagbobada+git@gmail.com>
2018-05-19 14:41:56 +0200

    Main : Give all possibilities to exit

--- src/parsing.c
+++ src/parsing.c
@@ -30,7 +30,7 @@ main() {
               Number, Operator, SExpr, QExpr, Expr, Lispy);
 
     puts("Lispy Version 0.0.0.1.0");
-    puts("Press Ctrl+C to exit\n");
+    puts("Press Ctrl+C or type \"exit ()\" in prompt to exit\n");
 
     struct lenv* e = lenv_new();
     lenv_add_builtins(e);
