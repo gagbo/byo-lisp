c6538102676bcab4fa882b24434f46f631e7f4d7
Gerry Agbobada <gagbobada+git@gmail.com>
2018-05-20 09:10:23 +0200

    Parsing : Handle the ^D gracefully
    We had a segmentation fault before since hitting ^D with an empty prompt
    returned input == NULL, so add_history had 'some issues'

--- src/parsing.c
+++ src/parsing.c
@@ -30,7 +30,7 @@ main() {
               Number, Operator, SExpr, QExpr, Expr, Lispy);
 
     puts("Lispy Version 0.0.1.1.0");
-    puts("Press Ctrl+C or type \"exit ()\" in prompt to exit\n");
+    puts("Press Ctrl+C, Ctrl+D, or type \"exit ()\" in prompt to exit\n");
 
     struct lenv* e = lenv_new();
     lenv_add_builtins(e);
@@ -38,6 +38,14 @@ main() {
     while (1) {
         char* input = readline("lispy> ");
 
+        /* Case where User input ^D on prompt */
+        if (NULL == input) {
+            printf(
+                "\n"
+                "End of input detected, exiting..."
+                "\n");
+            break;
+        }
         add_history(input);
 
         mpc_result_t r;
